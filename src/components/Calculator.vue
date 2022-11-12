7779627d2d2916719e4d29c609309aacfeb4a25a
Mingu Kim <mkim245@gmail.com>
2022-11-12 17:12:36 -0500

    make calculator shape

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,11 +4,48 @@ export default {
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="calculator">
+    <div class="display">100</div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn">/</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn">+</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn">-</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn">=</div>
   </div>
 </template>
 
 <style scoped>
+.calculator {
+  font-size:40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+.display {
+  grid-column: 1 / 5;
+  background-color: red;
+}
 
+.zero {
+  grid-column: 1 / 3;
+}
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #333;
+}
 </style>
