028e97bda42481d0ff5f21aa8c28232f32a84f45
Mingu Kim <mkim245@gmail.com>
2022-11-14 14:19:16 -0500

    add number click feature

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,8 +1,9 @@
 <script>
+
 export default {
   data() {
     return {
-      current: '123', 
+      current: '123',
     }
   },
   methods: {
@@ -11,35 +12,39 @@ export default {
     },
     sign() {
       this.current = this.current.charAt(0) === '-' ?
-      this.current.slice(1) : `-${this.current}`;
+        this.current.slice(1) : `-${this.current}`;
     },
     percent() {
       this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      this.current = `${this.current}${number}`;
     }
   }
 }
+
 </script>
 
 <template>
   <div class="calculator">
-    <div class="display">{{current || '0'}}</div>
+    <div class="display">{{ current || '0' }}</div>
     <div @click="clear" class="btn">AC</div>
     <div @click="sign" class="btn">+/–</div>
     <div @click="percent" class="btn">%</div>
     <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
     <div class="btn operator">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
     <div class="btn operator">–</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
     <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
+    <div @click="append('0')" class="btn zero">0</div>
     <div class="btn">.</div>
     <div class="btn operator">=</div>
   </div>
@@ -49,11 +54,12 @@ export default {
 .calculator {
   margin: 0 auto;
   width: 400px;
-  font-size:50px;
+  font-size: 50px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
 }
+
 .display {
   grid-column: 1 / 5;
   background-color: green;
@@ -73,6 +79,7 @@ export default {
   border-radius: 30%;
   text-align: center;
 }
+
 .operator {
   background-color: orange;
   color: white;
