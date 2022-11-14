2ed0cd676b1a73fd2bcb80f81b5153e205fe1269
Mingu Kim <mkim245@gmail.com>
2022-11-14 13:43:01 -0500

    code data and methods

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,35 +1,47 @@
 <script>
 export default {
+  data() {
+    return {
+      current: '123', 
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    }
+  }
 }
 </script>
 
 <template>
   <div class="calculator">
-    <div class="display">100</div>
-    <div class="btn">AC</div>
-    <div class="btn">+/-</div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">AC</div>
+    <div class="btn">+/–</div>
     <div class="btn">%</div>
-    <div class="btn">/</div>
+    <div class="btn operator">÷</div>
     <div class="btn">7</div>
     <div class="btn">8</div>
     <div class="btn">9</div>
-    <div class="btn">x</div>
+    <div class="btn operator">x</div>
     <div class="btn">4</div>
     <div class="btn">5</div>
     <div class="btn">6</div>
-    <div class="btn">+</div>
+    <div class="btn operator">–</div>
     <div class="btn">1</div>
     <div class="btn">2</div>
     <div class="btn">3</div>
-    <div class="btn">-</div>
+    <div class="btn operator">+</div>
     <div class="btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
 <style scoped>
 .calculator {
+  margin: 0 auto;
+  width: 400px;
   font-size:50px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -53,5 +65,9 @@ export default {
   border: 1px solid #333;
   border-radius: 30%;
   text-align: center;
+}
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
