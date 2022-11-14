91fd1b9e31735b47bc3d7a645b772fe98d273907
Mingu Kim <mkim245@gmail.com>
2022-11-14 16:38:11 -0500

    add calculation feature

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -3,7 +3,10 @@
 export default {
   data() {
     return {
+      previous: null,
       current: '123',
+      operator: null,
+      operatorClicked: false,
     }
   },
   methods: {
@@ -18,12 +21,43 @@ export default {
       this.current = `${parseFloat(this.current) / 100}`;
     },
     append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
       this.current = `${this.current}${number}`;
     },
     dot() {
       if (this.current.indexOf('.') === -1) {
         this.append('.');
       }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    multiply() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    subtract() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.previous),
+        parseFloat(this.current)
+      )}`;
+      this.previous = null;
     }
   }
 }
@@ -36,22 +70,22 @@ export default {
     <div @click="clear" class="btn">AC</div>
     <div @click="sign" class="btn">+/–</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">÷</div>
+    <div @click="divide" class="btn operator">÷</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">x</div>
+    <div @click="multiply" class="btn operator">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">–</div>
+    <div @click="subtract" class="btn operator">–</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot" class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 
